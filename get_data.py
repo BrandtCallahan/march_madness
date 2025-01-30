112508f2e9095d5e0a5b0cea75b653471f4673ad
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2025-01-29 21:40:30 -0600

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -45,16 +45,19 @@ from webscrape_utils import (
     # get_game_lineups,
     # get_game_gambling,
 )
+from team_dict import *
 
 """
 Team stats by game
 """
 season_year = 2025
 team = "vanderbilt"
+team_url = reference_dict[team]
+team_name = tmname_dict[team]
 
 
-def get_team_stats(season_year, team):
-    url = f"https://www.sports-reference.com/cbb/schools/{team}/men/{season_year}-gamelogs.html"
+def get_team_stats(season_year, team_url, team_name):
+    url = f"https://www.sports-reference.com/cbb/schools/{team_url}/men/{season_year}-gamelogs.html"
     # findTables(url)
 
     # team statistics
@@ -70,7 +73,12 @@ def get_team_stats(season_year, team):
     ].reset_index(drop=True)
 
     # label team
-    team_gamelog["Tm"] = team
+    team_gamelog["Tm"] = team_name
+
+    # add conference
+    tm_conf = conference_dict[f"{team_name}"]
+    team_gamelog["Conference"] = team_gamelog.Opp.map(conference_dict)
+    team_gamelog["Conf Game"] = team_gamelog["Conference"] == tm_conf
 
     # friendly home/away/neutral
     team_gamelog.loc[team_gamelog["Location"] == "@", "Home/Away"] = "Away"
@@ -115,13 +123,13 @@ def get_team_stats(season_year, team):
         team_gamelog["FGA"].astype(int)
         + (0.44 * team_gamelog["FTA"].astype(int))
         - team_gamelog["ORB"].astype(int)
-        + team_gamelog["TOV"].astype(int)
+        + team_gamelog["Opp TOV"].astype(int)
     ) / 2
     team_gamelog["Opp Poss"] = (
         team_gamelog["Opp FGA"].astype(int)
         + (0.44 * team_gamelog["Opp FTA"].astype(int))
         - team_gamelog["Opp ORB"].astype(int)
-        + team_gamelog["Opp TOV"].astype(int)
+        + team_gamelog["TOV"].astype(int)
     ) / 2
 
     # offense ratings
@@ -154,7 +162,7 @@ def get_team_stats(season_year, team):
 def heat_check(season_year, team, today_date):
 
     # team stats
-    team_df = get_team_stats(season_year, team)
+    team_df = get_team_stats(season_year, team, tmname_dict[team])
     team_df = team_df[
         (team_df["Date"].astype("datetime64[ns]")) < today_date.strftime("%Y-%m-%d")
     ].reset_index(drop=True)
@@ -177,6 +185,8 @@ def heat_check(season_year, team, today_date):
 
     # winning streak efficiency
     winstreak = team_df[team_df["Streak +/-"] >= 3]
+    w_off = winstreak.nlargest(int(len(winstreak)/2), "Off Eff")["Off Eff"].mean()
+    w_def = winstreak.nlargest(int(len(winstreak)/2), "Def Eff")["Def Eff"].mean()
     winstreak_tm = (
         winstreak.groupby(["Tm"])
         .agg(
@@ -192,12 +202,14 @@ def heat_check(season_year, team, today_date):
     )
 
     try:
-        winstreak_off = winstreak_tm["Off Eff"][0]
+        # winstreak_off = winstreak_tm["Off Eff"][0]
+        winstreak_off = w_off
     except KeyError:
         winstreak_off = nonstreak_tm["Off Eff"][0]
 
     try:
-        winstreak_def = winstreak_tm["Def Eff"][0]
+        # winstreak_def = winstreak_tm["Def Eff"][0]
+        winstreak_def = w_def
     except KeyError:
         winstreak_def = nonstreak_tm["Def Eff"][0]
 
@@ -216,7 +228,7 @@ def heat_check(season_year, team, today_date):
 def cool_down(season_year, team, today_date):
 
     # team stats
-    team_df = get_team_stats(season_year, team)
+    team_df = get_team_stats(season_year, team, tmname_dict[team])
     team_df = team_df[
         (team_df["Date"].astype("datetime64[ns]")) < today_date.strftime("%Y-%m-%d")
     ].reset_index(drop=True)
@@ -239,6 +251,8 @@ def cool_down(season_year, team, today_date):
 
     # losing streak efficiency
     lossstreak = team_df[team_df["Streak +/-"] <= -3]
+    l_off = lossstreak.nlargest(int(len(lossstreak)/2), "Off Eff")["Off Eff"].mean()
+    l_def = lossstreak.nlargest(int(len(lossstreak)/2), "Def Eff")["Def Eff"].mean()
     lossstreak_tm = (
         lossstreak.groupby(["Tm"])
         .agg(
@@ -254,12 +268,14 @@ def cool_down(season_year, team, today_date):
     )
 
     try:
-        lossstreak_off = lossstreak_tm["Off Eff"][0]
+        # lossstreak_off = lossstreak_tm["Off Eff"][0]
+        lossstreak_off = l_off
     except KeyError:
         lossstreak_off = nonstreak_tm["Off Eff"][0]
 
     try:
-        lossstreak_def = lossstreak_tm["Def Eff"][0]
+        # lossstreak_def = lossstreak_tm["Def Eff"][0]
+        lossstreak_def = l_def
     except KeyError:
         lossstreak_def = nonstreak_tm["Def Eff"][0]
 
@@ -275,136 +291,100 @@ def cool_down(season_year, team, today_date):
     return lossstreak_df
 
 
-def home_away_adj(season_year, team, today_date):
+def eff_compare(team, team_df, allgame_tm, location, conf_game):
+    if conf_game:
+        game = team_df[(team_df["Home/Away"] == f"{location}") & (team_df['Conf Game'] == conf_game)]
+    else:
+        game = team_df[
+            (team_df["Home/Away"] == f"{location}")
+    ]
+
+    tm = (
+        game.groupby(["Tm"])
+        .agg(
+            OffEff=("Off Eff", "mean"),
+            DefEff=("Def Eff", "mean"),
+        )
+        .reset_index()
+    ).rename(
+        columns={
+            "OffEff": "Off Eff",
+            "DefEff": "Def Eff",
+        }
+    )
+
+    try:
+        off_eff = tm["Off Eff"][0]
+    except KeyError:
+        off_eff = allgame_tm["Off Eff"][0]
+
+    try:
+        def_eff = tm["Def Eff"][0]
+    except KeyError:
+        def_eff = allgame_tm["Def Eff"][0]
+
+    # compare
+    eff_df = pd.DataFrame(
+        data={
+            "Tm": [team],
+            "Location": [f"{location}"],
+            "Loc Off Eff": [off_eff - allgame_tm["Off Eff"][0]],
+            "Loc Def Eff": [def_eff - allgame_tm["Def Eff"][0]],
+        }
+    ).reset_index(drop=True)
+
+    return eff_df
+
+
+def home_away_adj(season_year, team, today_date, conf_game):
 
     # team stats
-    team_df = get_team_stats(season_year, team)
+    team_df = get_team_stats(season_year, team, tmname_dict[team])
     team_df = team_df[
         (team_df["Date"].astype("datetime64[ns]")) < today_date.strftime("%Y-%m-%d")
     ].reset_index(drop=True)
 
     # avg. game efficiency (all games)
-    allgame_tm = (
-        team_df.groupby(["Tm"])
-        .agg(
-            OffEff=("Off Eff", "mean"),
-            DefEff=("Def Eff", "mean"),
+    if conf_game:
+        allgame_tm = (
+            team_df[team_df["Conf Game"]]
+            .groupby(["Tm"])
+            .agg(
+                OffEff=("Off Eff", "mean"),
+                DefEff=("Def Eff", "mean"),
+            )
+            .reset_index()
+        ).rename(
+            columns={
+                "OffEff": "Off Eff",
+                "DefEff": "Def Eff",
+            }
         )
-        .reset_index()
-    ).rename(
-        columns={
-            "OffEff": "Off Eff",
-            "DefEff": "Def Eff",
-        }
-    )
+    else:
+        allgame_tm = (
+            team_df
+            .groupby(["Tm"])
+            .agg(
+                OffEff=("Off Eff", "mean"),
+                DefEff=("Def Eff", "mean"),
+            )
+            .reset_index()
+        ).rename(
+            columns={
+                "OffEff": "Off Eff",
+                "DefEff": "Def Eff",
+            }
+        )
 
     # home game efficiency
-    hm_game = team_df[team_df["Home/Away"] == "Home"]
-    hm_tm = (
-        hm_game.groupby(["Tm"])
-        .agg(
-            OffEff=("Off Eff", "mean"),
-            DefEff=("Def Eff", "mean"),
-        )
-        .reset_index()
-    ).rename(
-        columns={
-            "OffEff": "Off Eff",
-            "DefEff": "Def Eff",
-        }
-    )
-
-    try:
-        hm_off = hm_tm["Off Eff"][0]
-    except KeyError:
-        hm_off = allgame_tm["Off Eff"][0]
-
-    try:
-        hm_def = hm_tm["Def Eff"][0]
-    except KeyError:
-        hm_def = allgame_tm["Def Eff"][0]
-
-    # compare
-    hm_df = pd.DataFrame(
-        data={
-            "Tm": [team],
-            "Location": ["Home"],
-            "Loc Off Eff": [hm_off - allgame_tm["Off Eff"][0]],
-            "Loc Def Eff": [hm_def - allgame_tm["Def Eff"][0]],
-        }
-    ).reset_index(drop=True)
+    hm_df = eff_compare(tmname_dict[team], team_df, allgame_tm, "Home", conf_game)
 
     # away game efficiency
-    aw_game = team_df[team_df["Home/Away"] == "Away"]
-    aw_tm = (
-        aw_game.groupby(["Tm"])
-        .agg(
-            OffEff=("Off Eff", "mean"),
-            DefEff=("Def Eff", "mean"),
-        )
-        .reset_index()
-    ).rename(
-        columns={
-            "OffEff": "Off Eff",
-            "DefEff": "Def Eff",
-        }
-    )
+    aw_df = eff_compare(tmname_dict[team], team_df, allgame_tm, "Away", conf_game)
 
-    try:
-        aw_off = aw_tm["Off Eff"][0]
-    except KeyError:
-        aw_off = allgame_tm["Off Eff"][0]
+    # neutral game efficiency
+    ne_df = eff_compare(tmname_dict[team], team_df, allgame_tm, "Neutral", conf_game)
 
-    try:
-        aw_def = aw_tm["Def Eff"][0]
-    except KeyError:
-        aw_def = allgame_tm["Def Eff"][0]
-
-    # compare
-    aw_df = pd.DataFrame(
-        data={
-            "Tm": [team],
-            "Location": ["Away"],
-            "Loc Off Eff": [aw_off - allgame_tm["Off Eff"][0]],
-            "Loc Def Eff": [aw_def - allgame_tm["Def Eff"][0]],
-        }
-    ).reset_index(drop=True)
-
-    # away game efficiency
-    ne_game = team_df[team_df["Home/Away"] == "Neutral"]
-    ne_tm = (
-        ne_game.groupby(["Tm"])
-        .agg(
-            OffEff=("Off Eff", "mean"),
-            DefEff=("Def Eff", "mean"),
-        )
-        .reset_index()
-    ).rename(
-        columns={
-            "OffEff": "Off Eff",
-            "DefEff": "Def Eff",
-        }
-    )
-
-    try:
-        ne_off = ne_tm["Off Eff"][0]
-    except KeyError:
-        ne_off = allgame_tm["Off Eff"][0]
-
-    try:
-        ne_def = ne_tm["Def Eff"][0]
-    except KeyError:
-        ne_def = allgame_tm["Def Eff"][0]
-
-    # compare
-    ne_df = pd.DataFrame(
-        data={
-            "Tm": [team],
-            "Location": ["Neutral"],
-            "Loc Off Eff": [ne_off - allgame_tm["Off Eff"][0]],
-            "Loc Def Eff": [ne_def - allgame_tm["Def Eff"][0]],
-        }
-    ).reset_index(drop=True)
 
     location_df = pd.concat([hm_df, aw_df, ne_df]).reset_index(drop=True)
 
