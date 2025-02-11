6d1f9818226b0b4a66f27d7b9dc0a3d4c63ea243
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2025-02-11 08:58:14 -0600

    Update team_dict.py

--- team_dict.py
+++ team_dict.py
@@ -1,58 +1,10 @@
 import bidict
+import pandas as pd
 
-tmname_dict = bidict.bidict(
-    alabama="Alabama",
-    arkansas="Arkansas",
-    auburn="Auburn",
-    florida="Florida",
-    georgia="Georgia",
-    kentucky="Kentucky",
-    lsu="LSU",
-    olemiss="Ole Miss",
-    mississippist="Mississppi State",
-    missouri="Missouri",
-    oklahoma="Oklahoma",
-    southcarolina="South Carolina",
-    tennessee="Tennessee",
-    texas="Texas",
-    texasam="Texas A&M",
-    vanderbilt="Vanderbilt",
-)
 
-reference_dict = bidict.bidict(
-    alabama="alabama",
-    arkansas="arkansas",
-    auburn="auburn",
-    florida="florida",
-    georgia="georgia",
-    kentucky="kentucky",
-    lsu="louisiana-state",
-    olemiss="mississippi",
-    mississippist="mississppi-state",
-    missouri="missouri",
-    oklahoma="oklahoma",
-    southcarolina="south-carolina",
-    tennessee="tennessee",
-    texas="texas",
-    texasam="texas-am",
-    vanderbilt="vanderbilt",
-)
+def get_teamnm():
+    team_df = pd.read_csv(
+        f"~/Documents/Python/professional_portfolio/march_madness/csv_files/team_df.csv"
+    )
 
-conference_dict = {
-    "Alabama": "SEC",
-    "Arkansas": "SEC",
-    "Auburn": "SEC",
-    "Florida": "SEC",
-    "Georgia": "SEC",
-    "Kentucky": "SEC",
-    "LSU": "SEC",
-    "Ole Miss": "SEC",
-    "Mississippi State": "SEC",
-    "Missouri": "SEC",
-    "Oklahoma": "SEC",
-    "South Carolina": "SEC",
-    "Tennessee": "SEC",
-    "Texas": "SEC",
-    "Texas A&M": "SEC",
-    "Vanderbilt": "SEC",
-}
+    return team_df
