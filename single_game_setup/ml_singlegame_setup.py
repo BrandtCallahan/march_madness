2163ddd5ee8560c413bccf49140935bd3b5c957e
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2025-06-04 22:42:15 -0500

    add other functions

--- single_game_setup/ml_singlegame_setup.py
+++ single_game_setup/ml_singlegame_setup.py
@@ -26,7 +26,327 @@ from sklearn.metrics import (
 from utils.get_data import *
 from utils.team_dict import *
 from utils.beautiful_soup_helper import *
-from ml_single_game.game_data import *
+
+
+def gamelog_setup(season, tm_name, gm_date):
+
+    # make sure 'today' is a date and not string
+    gm_date = pd.to_datetime(gm_date)
+
+    team_df = get_teamnm()
+    team_df = team_df[team_df["Season"] == season]
+
+    tm_df = team_df[team_df["Tm Name"] == tm_name].reset_index(drop=True)
+
+    # tm_url = f"https://www.sports-reference.com/cbb/schools/{tm_df['Ref Name'][0]}/men/{season}-gamelogs.html"
+    # team_gamelog = read_gamelog(tm_url)
+    # team_gamelog["Tm"] = tm_name
+
+    # temporary workaround
+    gamelog = pd.read_csv(
+        f"~/OneDrive - Tennessee Titans/Documents/Python/professional_portfolio/march_madness/csv_files/season{season}_tm_boxscores.csv",
+    )
+    team_gamelog = gamelog[gamelog["Tm"] == tm_df["Tm Name"][0]]
+
+    team_gamelog = team_gamelog[
+        (
+            ~(team_gamelog["Opp"].isin(["", "Opponent", "Opp"]))
+            & ~(team_gamelog["W/L"].isin(["", np.nan, pd.NA]))
+            & ~(team_gamelog["FG"].isin(["", np.nan, pd.NA]))
+            & ~(team_gamelog["G"].isin(["", np.nan, pd.NA]))
+        )
+    ].reset_index(drop=True)
+
+    # set dtypes
+    team_gamelog = team_gamelog.astype(
+        {
+            "G": int,
+            "Tm Score": int,
+            "Opp Score": int,
+            "FG": int,
+            "FGA": int,
+            "FG%": float,
+            "3P": int,
+            "3PA": int,
+            "3P%": float,
+            # "2P": int,
+            # "2PA": int,
+            # "2P%": float,
+            # "eFG%": float,
+            "FT": int,
+            "FTA": int,
+            "FT%": float,
+            "ORB": int,
+            # "DRB": int,
+            "TRB": int,
+            "AST": int,
+            "STL": int,
+            "BLK": int,
+            "TOV": int,
+            "PF": int,
+            "Opp FG": int,
+            "Opp FGA": int,
+            "Opp FG%": float,
+            "Opp 3P": int,
+            "Opp 3PA": int,
+            "Opp 3P%": float,
+            # "Opp 2P": int,
+            # "Opp 2PA": int,
+            # "Opp 2P%": float,
+            # "Opp eFG%": float,
+            "Opp FT": int,
+            "Opp FTA": int,
+            "Opp FT%": float,
+            "Opp ORB": int,
+            # "Opp DRB": int,
+            "Opp TRB": int,
+            "Opp AST": int,
+            "Opp STL": int,
+            "Opp BLK": int,
+            "Opp TOV": int,
+            "Opp PF": int,
+        },
+    )
+
+    # friendly win/loss
+    team_gamelog.loc[team_gamelog["W/L"].str.contains("W"), "W/L Flag"] = 1
+    team_gamelog["W/L Flag"] = team_gamelog["W/L Flag"].fillna(0)
+
+    team_gamelog["W/L"] = team_gamelog["W/L"].str[0]
+
+    # running sum W
+    team_gamelog["Total W"] = team_gamelog.groupby(["Tm"])["W/L Flag"].cumsum()
+    team_gamelog["Total W%"] = team_gamelog["Total W"] / team_gamelog["G"].astype(int)
+
+    # possessions
+    team_gamelog["Poss"] = (
+        team_gamelog["FGA"].astype(int)
+        + (0.44 * team_gamelog["FTA"].astype(int))
+        - team_gamelog["ORB"].astype(int)
+        + team_gamelog["TOV"].astype(int)
+    )
+    team_gamelog["Poss Ext"] = (
+        team_gamelog["FGA"].astype(int)
+        + (0.4 * team_gamelog["FTA"].astype(int))
+        - (
+            1.07
+            * (
+                team_gamelog["ORB"].astype(int)
+                / (
+                    team_gamelog["ORB"].astype(int)
+                    + (
+                        team_gamelog["Opp TRB"].astype(int)
+                        - team_gamelog["Opp ORB"].astype(int)
+                    )
+                )
+            )
+        )
+        * (team_gamelog["FGA"].astype(int) - team_gamelog["FG"].astype(int))
+        + team_gamelog["TOV"].astype(int)
+    )
+    team_gamelog["Opp Poss"] = (
+        team_gamelog["Opp FGA"].astype(int)
+        + (0.44 * team_gamelog["Opp FTA"].astype(int))
+        - team_gamelog["Opp ORB"].astype(int)
+        + team_gamelog["Opp TOV"].astype(int)
+    )
+    team_gamelog["Opp Poss Ext"] = (
+        team_gamelog["Opp FGA"].astype(int)
+        + (0.4 * (team_gamelog["Opp FTA"].astype(int)))
+        - (
+            1.07
+            * (team_gamelog["Opp ORB"].astype(int))
+            / (
+                team_gamelog["Opp ORB"].astype(int)
+                + (team_gamelog["TRB"].astype(int) - team_gamelog["ORB"].astype(int))
+            )
+        )
+        * (team_gamelog["Opp FGA"].astype(int) - team_gamelog["Opp FG"].astype(int))
+        + team_gamelog["Opp TOV"].astype(int)
+    )
+
+    # offense ratings
+    team_gamelog["Off Eff"] = (
+        team_gamelog["Tm Score"].astype(int) / team_gamelog["Poss Ext"]
+    ) * 100
+    team_gamelog["Shoot Eff"] = (
+        team_gamelog["FG"].astype(int) + (0.5 * team_gamelog["3P"].astype(int))
+    ) / team_gamelog["FGA"].astype(int)
+    team_gamelog["AST TOV Eff"] = team_gamelog["AST"].astype(int) / team_gamelog[
+        "TOV"
+    ].astype(int)
+
+    # defense ratings
+    team_gamelog["Def Eff"] = (
+        team_gamelog["Opp Score"].astype(int) / team_gamelog["Opp Poss Ext"]
+    ) * 100
+    team_gamelog["Opp Shoot Eff"] = (
+        team_gamelog["Opp FG"].astype(int) + (0.5 * team_gamelog["Opp 3P"].astype(int))
+    ) / team_gamelog["Opp FGA"].astype(int)
+    team_gamelog["Opp AST TOV Eff"] = team_gamelog["Opp AST"].astype(
+        int
+    ) / team_gamelog["Opp TOV"].astype(int)
+
+    # tm effieciency rating
+    team_gamelog["Tm Eff"] = team_gamelog["Off Eff"] - team_gamelog["Def Eff"]
+
+    # margin of victory
+    team_gamelog["Margin Victory"] = team_gamelog["Tm Score"].astype(
+        int
+    ) - team_gamelog["Opp Score"].astype(int)
+
+    # game luck
+    team_gamelog["Tm Luck"] = (
+        (team_gamelog["Tm Score"] / team_gamelog["Opp Score"]) ** 10.5
+    ) / (((team_gamelog["Tm Score"] / team_gamelog["Opp Score"]) ** 10.5) + 1)
+
+    # # flag conference games (regular season)
+    # team_gamelog.loc[(team_gamelog["Game Type"] == "REG (Conf)"), "Conf Game"] = 1
+    # team_gamelog["Conf Game"] = team_gamelog["Conf Game"].fillna(0)
+
+    # # rename OT to 0/1 flag
+    # team_gamelog["OT"] = (
+    #     team_gamelog["OT"].replace("OT", 1).replace(pd.NA, 0).astype(int)
+    # )
+
+    # rename Home/Away/Neutral to 2/1/0
+    team_gamelog["Location"] = (
+        team_gamelog["Location"].replace("@", 1).replace(pd.NA, 2).replace("N", 0)
+    ).astype(int)
+
+    # limit data to only show up to defined date (not including)
+    team_gamelog = (
+        team_gamelog[
+            (team_gamelog["Date"].astype("datetime64[ns]") < gm_date)
+        ].reset_index(drop=True)
+        # .drop(columns=["Rank"])
+    )
+
+    return team_gamelog
+
+
+def rolling_gamedata(season, hm_tm, aw_tm, gm_date):
+
+    # pull both team's gamelogs up to game
+    hm_gm_log = gamelog_setup(season, hm_tm, gm_date)
+    aw_gm_log = gamelog_setup(season, aw_tm, gm_date)
+
+    gm_log = pd.concat([hm_gm_log, aw_gm_log])
+
+    # group gamelog stats (season avg.)
+    gm_df = (
+        gm_log.groupby(["Tm"], observed=True)
+        .agg(
+            W=("W/L Flag", "sum"),
+            Wpct=("Total W%", "last"),
+            Poss=("Poss Ext", "median"),
+            OppPoss=("Opp Poss Ext", "median"),
+            OffEff=("Off Eff", "median"),
+            ShootEff=("Shoot Eff", "median"),
+            DefEff=("Def Eff", "median"),
+            OppShootEff=("Opp Shoot Eff", "median"),
+            TmLuckW=("Tm Luck", "sum"),
+            Pts=("Tm Score", "median"),
+            OppPts=("Opp Score", "median"),
+            # start boxscore stats
+            FGpct=("FG%", "median"),
+            P3pct=("3P%", "median"),
+            # eFGpct=("eFG%", "median"),
+            FTpct=("FT%", "median"),
+            ORB=("ORB", "median"),
+            # DRB=("DRB", "median"),
+            TRB=("TRB", "median"),
+            AST=("AST", "median"),
+            STL=("STL", "median"),
+            BLK=("BLK", "median"),
+            TOV=("TOV", "median"),
+            PF=("PF", "median"),
+            # opponent boxscore stats
+            OppFGpct=("FG%", "median"),
+            OppP3pct=("3P%", "median"),
+            # OppeFGpct=("eFG%", "median"),
+            OppFTpct=("FT%", "median"),
+            OppORB=("ORB", "median"),
+            # OppDRB=("DRB", "median"),
+            OppTRB=("TRB", "median"),
+            OppAST=("AST", "median"),
+            OppSTL=("STL", "median"),
+            OppBLK=("BLK", "median"),
+            OppTOV=("TOV", "median"),
+            OppPF=("PF", "median"),
+        )
+        .reset_index()
+        .rename(
+            columns={
+                "P3pct": "3Ppct",
+                "OppP3pct": "Opp3Ppct",
+            }
+        )
+    )
+    gm_df["TmLuck"] = gm_df["W"] - gm_df["TmLuckW"]
+    gm_df["Game Date"] = gm_date
+
+    # transform gm_df into single line game for game results df
+    hm = (
+        gm_df[gm_df["Tm"] == hm_tm]
+        .add_prefix("Hm_")
+        .rename(columns={"Hm_Game Date": "Game Date"})
+    )
+    aw = (
+        gm_df[gm_df["Tm"] == aw_tm]
+        .add_prefix("Aw_")
+        .rename(columns={"Aw_Game Date": "Game Date"})
+    )
+
+    full_gm_df = (aw.merge(hm, how="outer", on=["Game Date"])).reset_index(drop=True)
+    full_gm_df["Matchup"] = full_gm_df["Aw_Tm"] + " vs. " + full_gm_df["Hm_Tm"]
+
+    return full_gm_df
+
+
+def season_data(season):
+
+    # read results df
+    results_df = pd.read_csv(
+        f"~/OneDrive - Tennessee Titans/Documents/Python/professional_portfolio/march_madness/csv_files/season{season}_results.csv",
+    )
+
+    # for each result, run rolling_gamedata()
+    for n, matchup in enumerate(results_df["Matchup"].tolist()):
+        logger.info(
+            f"{n+1}/{len(results_df["Matchup"].tolist())}: {results_df["Matchup"].tolist()[n]}"
+        )
+
+        hm_tm = results_df["Home Team"][n]
+        aw_tm = results_df["Away Team"][n]
+        gm_date = results_df["Game Date"][n]
+
+        gamelog_stats = rolling_gamedata(season, hm_tm, aw_tm, gm_date)
+
+        gamelog_stats = gamelog_stats.merge(
+            results_df[
+                [
+                    "Matchup",
+                    "Home Team",
+                    "Away Team",
+                    "Home W",
+                    "Home Pt Diff",
+                ]
+            ],
+            how="inner",
+            left_on=["Matchup", "Aw_Tm", "Hm_Tm"],
+            right_on=["Matchup", "Away Team", "Home Team"],
+        )
+
+        season_df = pd.concat([season_df, gamelog_stats]).drop_duplicates().reset_index(drop=True)
+
+    # save .csv file
+    season_df.to_csv(
+        f"~/OneDrive - Tennessee Titans/Documents/Python/professional_portfolio/march_madness/csv_files/season{season}_matchup_results.csv",
+        index=False,
+    )
+
+    return season_df
 
 
 def single_game_model(data_seasons, today, matchup):
