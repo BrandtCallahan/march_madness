57ba22d316d6ce2e9ecaa31da1925c204d749ccf
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2025-06-06 19:31:26 -0500

    adding ml model for interactive bracket

--- cbb_marchmadness.py
+++ cbb_marchmadness.py
@@ -7,13 +7,14 @@ from march_madness_setup.interactive_march_madness_bracket import *
 season = 2025
 
 # manual input for date
-today = pd.to_datetime("2025-02-14").strftime("%Y-%m-%d")
+today = pd.to_datetime("2025-03-19").strftime("%Y-%m-%d")
 # automatic date (today)
 today = datetime.now().strftime("%Y-%m-%d")
 
 # March Madness automatic simulation
-bracketology_df = march_madness(season, today, 101)
+bracketology_df = march_madness(season, today, 501)
 
 # Interactive March Madness bracket
 #   User input
-bracketology_df = interactive_march_madness(season, today)
+modeltype = "Model"  # ('Simulation'/'Model')
+bracketology_df = interactive_march_madness(season, today, modeltype)
